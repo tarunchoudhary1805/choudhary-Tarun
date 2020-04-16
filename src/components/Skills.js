425b3106dcc13d3cd5500f7978b312815d1b3af5
Tarun Choudhary <57765697+tarunchoudhary1805@users.noreply.github.com>
2020-04-16 19:25:20 +0530

    Add files via upload

---
+++ src/components/Skills.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Skills() {
+    return (
+        <div>
+            <h1 className="SkillSet">SkillSet: HTML | CSS | JS | REACT-JS</h1>
+        </div>
+    )
+}
+
+export default Skills
---
+++ src/components/SocialLinks.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function SocialLinks() {
+    return (
+        <div >
+            <a href="https://github.com/tarunchoudhary1805" target="_blank" className="links" ><i class="fa fa-github" aria-hidden="true"></i></a>
+            <a href="https://www.linkedin.com/in/tarun-choudhary-072063190/" target="_blank" className="links"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+            <a href="https://www.facebook.com/tarun.choudhary.7106670" target="_blank" className="links"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            <a href="mailto:tarunchoudhary1805@gmail.com" target="_blank" className="links"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>
+            <a href="https://twitter.com/TARUNCH81407654" className="links" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            <a href="tel:9009808875" className="links"><i class="fa fa-mobile" aria-hidden="true"></i></a>
+        </div>
+    )
+}
+
+export default SocialLinks
